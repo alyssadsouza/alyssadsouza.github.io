592f718a799f5297913ddb8d8c390e5d883618fc
Alyssa D'Souza <alyssa.dsouza63@gmail.com>
2021-08-15 16:04:30 -0400

    Add nav animations

---
+++ animations.js
@@ -0,0 +1,28 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+//     var controller = new ScrollMagic.Controller();
+
+//     var welcome = new ScrollMagic.Scene({
+//         triggerElement: "#welcome",
+//         reverse:false
+//     })
+//     .setClassToggle("#welcome", "slide-in")
+//     .addIndicators()
+//     .addTo(controller)
+
+//     var about = new ScrollMagic.Scene({
+//         triggerElement: "#about",
+//         reverse:false
+//     })
+//     .setClassToggle("#about", "slide-in")
+//     .addIndicators()
+//     .addTo(controller)
+
+//     var projects = new ScrollMagic.Scene({
+//         triggerElement: "#projects",
+//         reverse:false
+//     })
+//     .setClassToggle("#projects", "slide-in")
+//     .addTo(controller)
+
+// })
--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    var currentPos = document.documentElement.scrollTop;
+    openNavAnimation();
+
+    // var currentPos = document.documentElement.scrollTop;
+    // var about = true;
+    // var projects = true;
+    // var aboutScroll = document.getElementById("about").scrollHeight - document.documentElement.scrollHeight*0.15;
+    // var projectScroll = document.getElementById("projects").scrollHeight - document.documentElement.scrollHeight*0.1;
 
     document.getElementById("menu-btn").addEventListener("click", () => {
 
@@ -22,84 +28,50 @@ document.addEventListener("DOMContentLoaded", function() {
 
         link.addEventListener("click", () => {
 
-            if (link.classList.contains("welcome-link")) {
-
-                closeWelcomeNav(link.id);
-                
-            } else {
-
-                closeMenu();
-
-            }
+            closeMenu(link.id);
     
         })
     })
 
-    // Nav animations:
+    // setInterval(() => {
 
-    document.getElementById("middle").addEventListener("click", () => {
+    //     if (document.documentElement.scrollTop >= aboutScroll && about) {
+    //         about = false;
+    //         setTimeout(() => {
 
-        // Slide up middle link and fade out other two
-        document.getElementById("top").classList.add("disappear");
-        document.getElementById("middle").classList.add("middle");
-        document.getElementById("bottom").classList.add("disappear");
+    //             openMenu(true);
 
-        setTimeout(function() {
-            // Replace inner links
-            var html = document.querySelector("#top").innerHTML;
-            moveContent("#middle", "#top");
-            moveContent("#bottom", "#middle");
-            document.querySelector("#bottom").innerHTML = html;
+    //             setTimeout(() => {
+    //                 closeMenu("middle");
+    //                 window.scrollTo(0, aboutScroll + document.documentElement.scrollHeight*0.08);
+    //             }, 2000);
 
-            document.getElementById("top").classList.remove("disappear");
-            document.getElementById("middle").classList.remove("middle");
-            document.getElementById("bottom").classList.remove("disappear");
+    //         }, 100);
+            
+    //     }
 
-            document.getElementById("middle").classList.add("hidden");
-            document.getElementById("bottom").classList.add("hidden");
+    // }, 100)
 
-            document.getElementById("top").classList.add("slide-up");
-            setTimeout(function() {
-                document.getElementById("main-nav").classList.add("hidden");
-                document.getElementById("middle").classList.remove("hidden");
-                document.getElementById("bottom").classList.remove("hidden");
-                document.getElementById("top").classList.remove("slide-up");
-            }, 750)
+    // setInterval(() => {
 
-        }, 750); 
-        
-    })
+    //     if (document.documentElement.scrollTop >= projectScroll && projects) {
+    //         projects = false;
+    //         setTimeout(() => {
 
-    document.getElementById("bottom").addEventListener("click", () => {
+    //             openMenu(true);
 
-        document.getElementById("top").classList.add("disappear");
-        document.getElementById("middle").classList.add("disappear");
-        document.getElementById("bottom").classList.add("bottom");
+    //             setTimeout(() => {
+    //                 closeMenu("middle");
+    //                 window.scrollTo(0, projectScroll + document.documentElement.scrollHeight*0.15);
+    //             }, 2000);
+
+    //         }, 100);
+            
+    //     }
+
+    // }, 100)
 
 
-        setTimeout(function() {
-            var html = document.querySelector("#top").innerHTML;
-            moveContent("#bottom", "#top");
-            moveContent("#middle", "#bottom");
-            document.querySelector("#middle").innerHTML = html;
-
-            document.getElementById("top").classList.remove("disappear");
-            document.getElementById("middle").classList.remove("disappear");
-            document.getElementById("bottom").classList.remove("bottom");
-
-            document.getElementById("middle").classList.add("hidden");
-            document.getElementById("bottom").classList.add("hidden");
-
-            document.getElementById("top").classList.add("slide-up");
-            setTimeout(function() {
-                document.getElementById("main-nav").classList.add("hidden");
-                document.getElementById("middle").classList.remove("hidden");
-                document.getElementById("bottom").classList.remove("hidden");
-                document.getElementById("top").classList.remove("slide-up");
-            }, 750)
-        }, 750); 
-        
-    })
 
 })
 
@@ -109,6 +81,8 @@ function moveContent(elem1, elem2) {
 
 function openMenu() {
 
+    // Hide sections
+
     document.querySelectorAll(".section").forEach(section => {
         section.classList.add("disappear");
     })
@@ -117,48 +91,241 @@ function openMenu() {
         document.querySelectorAll(".section").forEach(section => {
             section.classList.add("hidden");
         })
-        document.querySelectorAll(".section").forEach(section => {
-            section.classList.add("disappear");
-        })
+
+        // Rotate menu btn
 
         document.getElementById("menu-btn").classList.add("rotate-menu");
     
         setTimeout(function() {
 
             document.getElementById("main-nav").classList.remove("hidden");
+            openNavAnimation();
             document.querySelectorAll(".section").forEach(section => {
                 section.classList.remove("disappear");
             })
 
-        },1000)
-
-    }, 750)
-}
-
-function closeMenu() {
-    document.querySelectorAll(".section").forEach(section => {
-        section.classList.remove("hidden");
-    })
-
-    document.getElementById("menu-btn").classList.remove("rotate-menu");
-    document.getElementById("menu-btn").classList.add("rerotate-menu");
-
-    setTimeout(function() {
-        document.getElementById("main-nav").classList.add("hidden");
-        document.getElementById("menu-btn").classList.remove("rerotate-menu");
+        },750)
 
     }, 500)
 }
 
-function closeWelcomeNav(id) {
-    
-    document.querySelectorAll(".nav-link").forEach(link => {
-        link.classList.remove("welcome-link");
+function closeMenu(id) {
+
+    // Hide clicked section for animation
+
+    if (id != null) {
+        var clickedSection = document.getElementById(document.getElementById(id).firstElementChild.dataset.section);
+        clickedSection.classList.add("hide");
+    }
+
+    // Animation for link to slide in to top position and move link contents
+
+        if (id == "middle") {
+        middle("middle");
+    } else if (id == "bottom") {
+        bottom("bottom");
+    } else {
+        topLink();
+    }
+
+    // Re-add section content
+
+    document.querySelectorAll(".section").forEach(section => {
+        section.classList.remove("hidden");
     })
 
-    // (animations)
-
-    document.querySelector("#main-nav").classList.remove("welcome-nav");
-    document.querySelector("#menu-btn").classList.remove("hidden");
+    // Rotate menu btn
     
+    document.getElementById("menu-btn").classList.remove("rotate-menu");
+    document.getElementById("menu-btn").classList.add("rerotate-menu");
+
+    // Hide nav
+
+    setTimeout(function() {
+        document.getElementById("main-nav").classList.add("disappear");
+        document.getElementById("main-nav").classList.add("hidden");
+        document.getElementById("main-nav").classList.remove("disappear");
+        document.getElementById("menu-btn").classList.remove("rerotate-menu");
+
+        // Animation for section content to slide in
+
+        if (id != null) {
+            slideIn(clickedSection);
+        }
+
+    }, 1750)
+}
+
+// ----------------------------------------------------------------
+
+// Nav animations:
+
+function slideIn(clickedSection) {
+    setTimeout(() => {
+        clickedSection.classList.add("slide-in");
+        
+        setTimeout(() => {
+            clickedSection.classList.remove("slide-in");
+            clickedSection.classList.remove("hide");
+        }, 1000)
+
+    }, 750)
+}
+
+function openNavAnimation() {
+
+    document.getElementById("main-nav").classList.add("openNav");
+
+    setTimeout(() => {
+        document.getElementById("main-nav").classList.remove("openNav");
+    }, 1000)
+
+}
+
+function closeNavAnimation() {
+
+    document.getElementById("top").classList.add("disappear");
+    setTimeout(function() {
+        document.getElementById("main-nav").classList.add("hidden");
+        document.getElementById("middle").classList.remove("hidden");
+        document.getElementById("bottom").classList.remove("hidden");
+        document.getElementById("contact-links").classList.remove("hidden");
+        document.getElementById("top").classList.remove("disappear");
+    }, 750)
+}
+
+function topLink() {
+
+    // Position top link and fade out other two
+
+    document.getElementById("middle").classList.add("disappear");
+    document.getElementById("bottom").classList.add("disappear");
+    document.getElementById("contact-links").classList.add("disappear");
+
+    setTimeout(function() {
+        
+        document.getElementById("middle").classList.add("hide");
+        document.getElementById("bottom").classList.add("hide");
+        document.getElementById("contact-links").classList.add("hide");
+
+        document.getElementById("middle").classList.remove("disappear");
+        document.getElementById("bottom").classList.remove("disappear");
+        document.getElementById("contact-links").classList.remove("disappear");
+
+        setTimeout(() => {
+
+            document.getElementById("middle").classList.remove("hide");
+            document.getElementById("bottom").classList.remove("hide");
+            document.getElementById("contact-links").classList.remove("hide");
+
+            document.getElementById("middle").classList.add("hidden");
+            document.getElementById("bottom").classList.add("hidden");
+            document.getElementById("contact-links").classList.add("hidden");
+
+            // Animation to close nav
+            
+            closeNavAnimation();
+
+        }, 750)
+
+    }, 500); 
+}
+
+function middle(animation) {
+
+    // Position middle link and fade out other two
+
+    document.getElementById("top").classList.add("disappear");
+    document.getElementById("bottom").classList.add("disappear");
+    document.getElementById("contact-links").classList.add("disappear");
+
+    setTimeout(function() {
+        
+        document.getElementById("top").classList.add("hide");
+        document.getElementById("middle").classList.add(animation);
+        document.getElementById("bottom").classList.add("hide");
+        document.getElementById("contact-links").classList.add("hide");
+
+        document.getElementById("top").classList.remove("disappear");
+        document.getElementById("bottom").classList.remove("disappear");
+        document.getElementById("contact-links").classList.remove("disappear");
+
+        setTimeout(() => {
+
+            document.getElementById("top").classList.remove("hide");
+            document.getElementById("bottom").classList.remove("hide");
+            document.getElementById("contact-links").classList.remove("hide");
+
+            // Replace inner links
+
+            var html = document.querySelector("#top").innerHTML;
+            moveContent("#middle", "#top");
+            moveContent("#bottom", "#middle");
+            document.querySelector("#bottom").innerHTML = html;
+
+            // Remove animation classes
+
+            document.getElementById("middle").classList.remove(animation);
+
+            document.getElementById("middle").classList.add("hidden");
+            document.getElementById("bottom").classList.add("hidden");
+            document.getElementById("contact-links").classList.add("hidden");
+
+            // Animation to close nav
+            
+            closeNavAnimation();
+
+        }, 750)
+
+    }, 500); 
+}
+
+function bottom(animation) {
+    
+    
+    // Position bottom link and fade out other two
+
+    document.getElementById("top").classList.add("disappear");
+    document.getElementById("middle").classList.add("disappear");
+    document.getElementById("contact-links").classList.add("disappear");
+
+    setTimeout(() => {
+
+        document.getElementById("top").classList.add("hide");
+        document.getElementById("middle").classList.add("hide");
+        document.getElementById("bottom").classList.add(animation);
+        document.getElementById("contact-links").classList.add("hide");
+
+        document.getElementById("top").classList.remove("disappear");
+        document.getElementById("middle").classList.remove("disappear");
+        document.getElementById("contact-links").classList.remove("disappear");
+
+        setTimeout(function() {
+
+            document.getElementById("middle").classList.remove("hide");
+            document.getElementById("top").classList.remove("hide");
+            document.getElementById("contact-links").classList.remove("hide");
+
+            // Replace inner links
+    
+            var html = document.querySelector("#top").innerHTML;
+            moveContent("#bottom", "#top");
+            moveContent("#middle", "#bottom");
+            document.querySelector("#middle").innerHTML = html;
+    
+            // Remove animation classes
+    
+            document.getElementById("bottom").classList.remove(animation);
+    
+            document.getElementById("middle").classList.add("hidden");
+            document.getElementById("bottom").classList.add("hidden");
+            document.getElementById("contact-links").classList.add("hidden");
+            
+            // Animation to close nav
+    
+            closeNavAnimation();
+    
+        }, 750);
+
+    },500)
 }
