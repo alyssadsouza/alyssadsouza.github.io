77a1431c89c8e4bc643f8d1b9d354195f9e038b4
alyssadsouza <alyssa.dsouza63@gmail.com>
2022-08-01 12:35:12 -0400

    refactor Typewriter

---
+++ src/components/Typewriter.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+
+const Typewriter = ({headers, restartingIndex = 0}) => {
+  /* Typewriter effect */
+  const [header, setHeader] = useState("");
+  const [adding, setAdding] = useState(true);
+  const [headerText, setHeaderText] = useState(headers[0]);
+  const [index, setIndex] = useState(0);
+  const [headerIndex, setHeaderIndex] = useState(headers.length === 1 ? 0 : 1);
+
+  useEffect(() => {
+    if (adding) {
+      if (index <= headerText.length) {
+        setHeader(headerText.slice(0, index));
+        setTimeout(() => setIndex(index + 1), 75);
+      }
+      if (index === headerText.length) {
+        setTimeout(() => {
+          setAdding(false);
+          setIndex(index - 1);
+        }, 7500); // hold on full header for 7.5 seconds
+      }
+    } // there's a period between adding still being true but index > text length where text holds for a few seconds
+    else {
+      if (index >= restartingIndex) {
+        setHeader(headerText.slice(0, index));
+        if (index === restartingIndex) {
+          setAdding(true);
+          setHeaderText(headers[headerIndex]);
+          if (headers.length === 1 || headerIndex === headers.length - 1) {
+            setHeaderIndex(0);
+          } else {
+            setHeaderIndex(headerIndex + 1);
+          }
+          setTimeout(() => setIndex(index + 1), 300);
+        } else {
+          setTimeout(() => setIndex(index - 1), 75);
+        }
+      }
+    }
+  }, [index]);
+
+  return (
+    <>
+      {header}
+      <span className="caret"></span>
+    </>
+  );
+};
+
+export default Typewriter;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,58 +1,24 @@
 import { useState, useEffect } from "react";
 import NavArrow from '../components/NavArrow';
+import Typewriter from '../components/Typewriter';
 
 function Home({setMenuItem}) {
-  /* Typewriter effect */
-  const headers = [
-    "I'm a software engineering undergrad student.",
-    "I'm a self-taught web developer.",
-    "I'm a second-year undergrad.",
-  ];
-  const [header, setHeader] = useState("");
-  const [adding, setAdding] = useState(true);
-  const [headerText, setHeaderText] = useState(headers[0]);
-  const [index, setIndex] = useState(0);
-  const [headerIndex, setHeaderIndex] = useState(1);
-
-  useEffect(() => {
-    if (adding) {
-      if (index <= headerText.length) {
-        setHeader(headerText.slice(0, index));
-        setTimeout(() => setIndex(index + 1), 75);
-      }
-      if (index === headerText.length) {
-        setTimeout(() => {
-          setAdding(false);
-          setIndex(index - 1);
-        }, 7500); // hold on full header for 5 seconds
-      }
-    } // there's a period between adding still being true but index > text length where text holds for a few seconds
-    else {
-      if (index >= 6) {
-        // start from 6 again which is where "I'm a " ends
-        setHeader(headerText.slice(0, index));
-        if (index === 6) {
-          setAdding(true);
-          setHeaderText(headers[headerIndex]);
-          if (headerIndex === headers.length - 1) {
-            setHeaderIndex(0);
-          } else {
-            setHeaderIndex(headerIndex + 1);
-          }
-          setTimeout(() => setIndex(index + 1), 300);
-        } else {
-          setTimeout(() => setIndex(index - 1), 75);
-        }
-      }
-    }
-  }, [index]);
 
   return (
     <section id="home" className="flex flex-col justify-center lg:h-[90%] lg:overflow-y-auto w-full">
       <div id="home-text" className="flex flex-col w-full px-[10%] py-[5%] my-[15%]">
         <h1 className="text-3xl font-display font-bold">I'm Alyssa.</h1>
-        <h2 className="text-2xl font-display my-4">{header}<span className="caret"></span></h2>
-        <p className="">
+        <h2 className="text-2xl font-display my-4">
+          <Typewriter
+            headers={[
+              "I'm a software engineering undergrad student.",
+              "I'm a self-taught web developer.",
+              "I'm a second-year undergrad.",
+            ]}
+            restartingIndex={6} // start from 6 again which is where "I'm a " ends
+          />
+        </h2>
+        <p>
           I'm a second-year at the University of Waterloo and as a co-op student
           I’m actively looking for web development internships. This portfolio
           showcases my skills and some of the projects I’ve worked on.
