86c7ce3530085f69f76860af4d70b9722b8faf24
alyssadsouza <alyssa.dsouza63@gmail.com>
2023-01-29 20:39:34 -0500

    Set svg width height to fix Safari rendering bug

--- src/assets/about-image.js
+++ src/assets/about-image.js
@@ -2,7 +2,7 @@ import Typewriter from "../components/Typewriter";
 
 export default function AboutImage() {
   return (
-    <svg viewBox="0 0 1402 704" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width={"100%"} height={"100%"} viewBox="0 0 1402 704" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g id="About" clipPath="url(#clip0_434_2055)">
         <path
           id="path245-4"
